9636e300b70b8cebe1989a917c7a4d00934fda1f
Jakub <kubapodles468@gmail.com>
2024-07-28 15:45:52 +0200

    Added tagline and minor fixes

--- board-games-forum/src/components/AppHeader.vue
+++ board-games-forum/src/components/AppHeader.vue
@@ -1,9 +1,7 @@
-<!-- src/components/AppHeader.vue -->
-
 <template>
   <header>
     <h1>Board Game Central</h1>
-
+    <p class="tagline">Your Gateway to Endless Board Game Adventures and Strategies</p>
   </header>
 </template>
 
@@ -15,8 +13,22 @@ export default {
 
 <style scoped>
 header {
-  color: white;
-  padding: 10px;
+  padding: 40px;
   text-align: center;
+  margin-top: 7%;
+}
+
+h1 {
+  margin: 0;
+  font-size: 5em;
+  color: #fff;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+
+.tagline {
+  font-size: 1.5em;
+  margin-top: 10px;
+  color: #d3d3d3;
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5);
 }
 </style>
